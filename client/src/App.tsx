4f989fe8ba734069398f4bc34d8f25cf8213cc1c
pagimos <mohamed.islam895@gmail.com>
2024-10-20 23:58:13 +0100

    comming soon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+import { Facebook, Instagram, Send, Linkedin, Twitter } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Set a fixed end time for the countdown
+  const fixedEndTime: number = new Date("2024-12-01T00:00:00Z").getTime();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="container">
+        <div className="clock_content">
+          <h1>INTELCULATOR.COM</h1>
+          <h2 className="disc">
+            Intelculator is an IQ calculator website that provides accurate IQ
+            results through interactive intelligence tests, helping users assess
+            their cognitive abilities.
+          </h2>
+          <h3>Coming soon, stay tuned for something amazing!</h3>
+
+          <FlipClockCountdown
+            className="flip-clock"
+            to={fixedEndTime}
+            labels={["DAYS", "HOURS", "MINUTES", "SECONDS"]}
+            duration={0.5}
+          />
+
+          <button className="button">Notify Me</button>
+          <h6>
+            This Website is being built by{" "}
+            <a
+              href="https://www.instagram.com/pagimos/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @pagimos
+            </a>
+          </h6>
+
+          <div className="socials">
+            <a
+              href="https://www.instagram.com/intelculator/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Instagram size={32} color="#ffffff" className="social-icon" />
+            </a>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
