f40eedec1a6591843d075347956fb43229c48e33
pagimos <mohamed.islam895@gmail.com>
2025-02-09 17:00:14 +0100

    ++

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Ensures correct routing
+  server: {
+    fs: {
+      allow: ["."], // Allows all files to be accessed
+    },
+  },
 });
